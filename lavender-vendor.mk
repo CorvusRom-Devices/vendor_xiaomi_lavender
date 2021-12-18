0d3fa4e35fb154cdcac19c6250bd1c769612f220
wHo-EM-i <ehteshammalik18998@gmail.com>
2021-12-18 02:28:26 -0800

    lavender: drop QTI datastatusnotification
    Signed-off-by: MukulSharma06175 <sharmamukul06175@gmail.com>

--- lavender-vendor.mk
+++ lavender-vendor.mk
@@ -1818,7 +1818,6 @@ PRODUCT_PACKAGES += \
     TimeService \
     PowerOffAlarm \
     QtiTelephonyService \
-    datastatusnotification \
     imssettings \
     uceShimService \
     dpmserviceapp \
